6a080f70abefd4c892911f874953ac1dd2156abe
Kanogor <the.kanogor@yandex.ru>
2024-04-26 13:20:45 +0300

    Первый экран готов

--- core/src/main/java/ru/kanogor/core/base/BaseViewModel.kt
+++ core/src/main/java/ru/kanogor/core/base/BaseViewModel.kt
@@ -1,13 +1,8 @@
 package ru.kanogor.core.base
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
 abstract class BaseViewModel : ViewModel() {
 
--- feature_airtickets/src/main/java/ru/kanogor/feature_airtickets/di/FeatureAirticketsModule.kt
+++ feature_airtickets/src/main/java/ru/kanogor/feature_airtickets/di/FeatureAirticketsModule.kt
@@ -2,17 +2,16 @@ package ru.kanogor.feature_airtickets.di
 
 import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
 import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import ru.kanogor.feature_airtickets.data.PreferencesManager
 import ru.kanogor.feature_airtickets.data.repository.AirticketsRepositoryImpl
+import ru.kanogor.feature_airtickets.domain.model.LocalStorage
 import ru.kanogor.feature_airtickets.domain.repository.AirticketsRepository
 import ru.kanogor.feature_airtickets.domain.use_case.GetAirticketsUseCase
 import ru.kanogor.feature_airtickets.domain.use_case.impl.GetAirticketsUseCaseImpl
 import ru.kanogor.feature_airtickets.presentation.AirticketsViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-import ru.kanogor.feature_airtickets.data.PreferencesManager
-import ru.kanogor.feature_airtickets.domain.model.LocalStorage
 
 fun featureAirticketsModule() = module {
 
